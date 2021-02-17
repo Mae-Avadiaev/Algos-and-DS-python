e91c1e14743bfc1beb6e928403e3f792a2a36379
Mitya Avadyaev <mitya.ninja@yandex.ru>
2021-02-17 20:42:42 +0300

    Add files via upload

--- Yandex contest/C_PearsBasket.py
+++ Yandex contest/C_PearsBasket.py
@@ -1,63 +1,31 @@
-from abc import ABC, abstractmethod
+class PearsBasket():
+    def __init__(self, amount):
+        self.amount = amount
 
+    def __str__(self):
+        return str(self.amount)
 
-class Transport(ABC):
-    def __init__(self, number, type, speed):
-        self.number = number
-        self.type = type
-        self.speed = speed
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.amount})"
 
-    def countDistance(self, time, cycle):
-        return abs(cycle - time * self.speed) % cycle
+    def __floordiv__(self, other):
+        resList = list()
+        basketAmount = self.amount // other
+        rem = self.amount % other
+        for i in range(other):
+            resList.append(PearsBasket(basketAmount))
+        resList.append(PearsBasket(rem))
+        return resList
 
+    def __mod__(self, other):
+        self.amount = self.amount % other
+        return self.amount
 
-class Carriage(Transport):
-    def __init__(self, number, type, speed):
-        super().__init__(number, type, speed)
+    def __add__(self, other):
+        return PearsBasket(self.amount + other.amount)
 
-
-class Auto(Transport):
-    def __init__(self, number, type, speed, fuel):
-        super().__init__(number, type, speed)
-        if fuel == 98:
-            self.speed = speed
-        elif fuel == 95:
-            self.speed = int(speed / 100 * 90)
-        elif fuel == 92:
-            self.speed = int(speed / 100 * 80)
-
-
-class Moto(Transport):
-    def __init__(self, number, type, speed, fuel):
-        super().__init__(number, type, speed)
-        if fuel == 98:
-            self.speed = speed
-        elif fuel == 95:
-            self.speed = int(speed / 100 * 80)
-        elif fuel == 92:
-            self.speed = int(speed / 100 * 60)
-
-
-if __name__ == "__main__":
-    n, m, t = map(int, input().split())
-    mass = []
-    for i in range(n):
-        a = list(map(int, input().split()))
-        if a[1] == 1:
-            mass.append(Auto(a[0], a[1], a[2], a[3]))
-        elif a[1] == 2:
-            mass.append(Moto(a[0], a[1], a[2], a[3]))
+    def __sub__(self, other):
+        if self.amount - other < 0:
+            self.amount = 0
         else:
-            mass.append(Carriage(a[0], a[1], a[2]))
-
-    max1 = 0
-    winner = 0
-    for i, transport in enumerate(mass):
-        if (transport.speed * t) % m > max1:
-            max1 = (transport.speed * t) % m
-            winner = i
-        elif (transport.speed * t) % m == max1:
-            if transport.number < mass[winner].number:
-                max1 = (transport.speed * t) % m
-                winner = i
-    print(mass[winner].number)
+            self.amount = self.amount - other
